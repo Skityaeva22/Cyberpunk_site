648c8b0a84fc6555248b9e93052ba61fb46db061
a.skitiaeva <a.skitiaeva@bars-open.ru>
2025-04-15 09:35:13 +0500

    Модалка

--- index.js
+++ index.js
@@ -33,13 +33,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const modalText = document.getElementById('modalText');
     const closeModal = document.getElementById('closeModal');
 
-    // Открытие модального окна с текстом
     document.getElementById('moreInfoBtn').addEventListener('click', () => {
-        modalText.textContent = 'Чуть больше информации';
+        modalText.textContent = 'Информации нет :)';
         modal.style.display = 'flex';
     });
 
-    // Открытие модального окна с рандомным пользователем
     document.getElementById('privacyPolicyBtn').addEventListener('click', async () => {
         try {
             const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -47,25 +45,23 @@ document.addEventListener('DOMContentLoaded', () => {
             const randomUser = users[Math.floor(Math.random() * users.length)];
 
             modalText.innerHTML = `
-                Узнавать у 
-                <strong>${randomUser.name}</strong><br>
+                Данные 
+                <strong>${randomUser.name}</strong>:<br>
                 Email: ${randomUser.email}<br>
                 Phone: ${randomUser.phone}<br>
                 Website: ${randomUser.website}
             `;
             modal.style.display = 'flex';
         } catch (err) {
-            modalText.textContent = 'Ошибка загрузки данных.';
+            modalText.textContent = 'Ошибка загрузки данных';
             modal.style.display = 'flex';
         }
     });
 
-    // Закрытие модального окна
     closeModal.addEventListener('click', () => {
         modal.style.display = 'none';
     });
 
-    // Закрытие по клику вне модального окна
     window.addEventListener('click', (e) => {
         if (e.target === modal) {
             modal.style.display = 'none';
